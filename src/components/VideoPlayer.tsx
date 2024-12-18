5d2fa8e9beef3b95dc6af5d9420f4f92bc5b217a
vitaly-xymatic <vitaly.grinko@xymatic.com>
2024-12-18 11:57:55 +0100

    version-bump player, provide example and update integration

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,34 +1,38 @@
-import { GreenVideo } from '@xymatic/green-video-player-react';
+import { type FC } from 'react';
 import dynamic from 'next/dynamic';
-import { useRef, type FC } from 'react';
+
+import templateData from '../data/sampleTemplateData.json';
+import content from '../data/sampleContent.json';
 
 const GreenVideoPlayer = dynamic(() => import('./VideoPlayerLoader'), { ssr: false });
 
 export const VideoPlayer: FC = () => {
-  const ref = useRef<GreenVideo | null>(null);
+  const onEvent = (e: CustomEvent) => {
+    switch (e.detail.type) {
+      case 'impression': {
+        console.log('impression!');
+        break;
+      }
+      case 'ms_25': {
+        alert('25% milestone reached!');
+        break;
+      }
+      default: {
+        console.log(e.detail.type);
+      }
+    }
+  };
 
-  const playerConfig = {
-    autostartDelay: 0,
-    embedBorderRadius: 20,
-  };
-  const onPlayerReady = () => {
-    console.log('ready!');
-    const player = ref.current;
-    // work with Player API here
-    setTimeout(() => player?.video.pause(), 3000);
-    setTimeout(() => player?.video.play(), 7000);
-  };
+  // example: providing content and config statically, subscribing to player events
   return (
-    <div style={{ width: '640px', height: '360px' }}>
+    <div style={{ width: '640px' }}>
       <GreenVideoPlayer
-        forwardRef={(node: GreenVideo) => {
-          ref.current = node;
-        }}
-        embedId="Is4nVmpe"
         licenseKey="be132c877a4e0d3cf874074f4d9bdf7885c611a1"
-        contentId="de1d6c7997a06607772bb21cf8bf4b7ab5415fde"
-        config={playerConfig}
-        onReady={onPlayerReady}
+        content={content}
+        config={{
+          templateData,
+        }}
+        onEvent={onEvent}
       />
     </div>
   );
